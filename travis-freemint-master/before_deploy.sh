b1620d7269ba39a04f922029a430c9fc96dfccdd
Miro Kropacek <miro.kropacek@gmail.com>
2018-10-17 13:59:02 +0200

    Publish the longer version string

--- travis-freemint-master/before_deploy.sh
+++ travis-freemint-master/before_deploy.sh
@@ -4,8 +4,18 @@
 # -x: Display expanded script commands
 
 VERSION=$(/tmp/version)
-sed -i -e "s/MINT_MAJ_VERSION-MINT_MIN_VERSION-MINT_PATCH_LEVEL/${VERSION}/g;" .travis/bintray.desc
+MINT_MAJ_VERSION=$(echo ${VERSION} | cut -d - -f 1)
+MINT_MIN_VERSION=$(echo ${VERSION} | cut -d - -f 2)
+MINT_PATCH_LEVEL=$(echo ${VERSION} | cut -d - -f 3)
+
 TRAVIS_COMMIT=$(echo ${TRAVIS_COMMIT} | cut -c 1-8)
+
+if [ ${#MINT_PATCH_LEVEL} -eq 3 ]
+then
+	MINT_PATCH_LEVEL=${TRAVIS_COMMIT}
+fi
+
+sed -i -e "s/MINT_MAJ_VERSION-MINT_MIN_VERSION-MINT_PATCH_LEVEL/${MINT_MAJ_VERSION}-${MINT_MIN_VERSION}-${MINT_PATCH_LEVEL}/g;" .travis/bintray.desc
 sed -i -e "s/COMMIT_ID/${TRAVIS_COMMIT}/g;" .travis/bintray.desc
 USER=$(echo "${TRAVIS_REPO_SLUG}" | cut -d '/' -f 1)
 PROJECT=$(echo "${TRAVIS_REPO_SLUG}" | cut -d '/' -f 2)
