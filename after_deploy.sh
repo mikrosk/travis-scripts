20db6139e4aa67e8b3b4eb0813dc5432214b1080
Miro Kropacek <miro.kropacek@gmail.com>
2019-03-30 17:59:37 +0100

    Try to upload it into a version folder

--- after_deploy.sh
+++ after_deploy.sh
@@ -3,6 +3,9 @@
 # -u: Treat unset variables as an error when substituting.
 # -x: Display expanded script commands
 
+HERE="$(dirname "$0")"
+. "$HERE/version.sh"
+
 API="https://api.bintray.com"
 SUBJECT="freemint"
 REPO="freemint"
@@ -23,5 +26,5 @@ do
     --user ${BINTRAY_USER}:${BINTRAY_API_KEY} \
     --header "Content-Type: application/json" \
     --data "{ \"list_in_downloads\": true }" \
-    "${API}/file_metadata/${SUBJECT}/${REPO}/${PACKAGE_NAME}/$(basename $f)"
+    "${API}/file_metadata/${SUBJECT}/${REPO}/${PACKAGE_NAME}/${LONG_VERSION}/$(basename $f)"
 done
