b000dc2fbf29768dfbcae0720a508e114190efea
Miro Kropacek <miro.kropacek@gmail.com>
2019-11-03 14:11:54 +0100

    Keep all install actions at one place

--- install.sh
+++ install.sh
@@ -29,6 +29,17 @@ unzip "$QED_NAME"
 rm "$QED_NAME"
 cd -
 
-if test -f .travis/install-udo.sh; then
-	. .travis/install-udo.sh
-fi
+mkdir -p ~/"tmp/udo"
+cd ~/"tmp/udo"
+wget "http://www.tho-otto.de/download/udo-7.12-linux.tar.bz2"
+tar xjf udo-7.12-linux.tar.bz2
+UDO=~/"tmp/udo/udo"
+export UDO
+cd -
+
+cd ~/"tmp"
+wget "http://tho-otto.de/download/hcp-1.0.3-linux.tar.bz2"
+tar xjf hcp-1.0.3-linux.tar.bz2
+HCP=~/"tmp/hcp-1.0.3/bin/hcp"
+export HCP
+cd -
