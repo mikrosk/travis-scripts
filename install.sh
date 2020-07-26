435e69e79c9524922b0ec5fe8dfe5edff526b031
Miro Kropacek <miro.kropacek@gmail.com>
2020-07-26 22:27:35 +0200

    Experimental support for PRs

--- install.sh
+++ install.sh
@@ -3,83 +3,81 @@
 # -u: Treat unset variables as an error when substituting.
 # -x: Display expanded script commands
 
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ]
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_REPO_SLUG}" = "freemint/freemint" ]
 then
-CURL_HEADER="Authorization: token $GITHUB_OATH_TOKEN"
-CURL_USER="${BINTRAY_USER}:${BINTRAY_API_KEY}"
+	CURL_HEADER="Authorization: token $GITHUB_OATH_TOKEN"
+	CURL_USER="${BINTRAY_USER}:${BINTRAY_API_KEY}"
+
+	if [ "$CPU_TARGET" = "ara" ]
+	then
+		CPU_TARGET="040"
+	elif [ "$CPU_TARGET" = "prg" ]
+	then
+		CPU_TARGET="000"
+	fi
+
+	LIBCMINI_URL=$(curl -s -H "$CURL_HEADER" https://api.github.com/repos/freemint/libcmini/releases/latest | jq -r '.assets[].browser_download_url')
+
+	cd ..
+	mkdir libcmini
+	wget -q -O - "$LIBCMINI_URL" | tar xzf - -C libcmini
+	cd -
+
+	cd .travis
+	for package in toswin2 qed cops hypview
+	do
+		unset PACKAGE_VERSION
+		unset PACKAGE_PATH
+		PACKAGE_VERSION=$(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/freemint/$package" | jq -r '.latest_version')
+		read PACKAGE_PATH \
+			<<< $(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/freemint/$package/versions/$PACKAGE_VERSION/files" \
+				| jq -r '.[] | select(.name | endswith("-" + env.CPU_TARGET + ".zip")) | .path')
+		wget -q -O temp.zip "https://dl.bintray.com/freemint/freemint/$PACKAGE_PATH" && unzip -q temp.zip && rm temp.zip
+	done
+	cd -
+
+	sudo mkdir -p "/usr/m68k-atari-mint"
+	cd "/usr/m68k-atari-mint"
+	for package in mintlib fdlibm gemlib cflib gemma
+	do
+		unset PACKAGE_VERSION
+		unset PACKAGE_PATH
+		PACKAGE_VERSION=$(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/freemint/$package" | jq -r '.latest_version')
+		read PACKAGE_PATH \
+			<<< $(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/freemint/$package/versions/$PACKAGE_VERSION/files" \
+				| jq -r '.[].path')
+		wget -q -O - "https://dl.bintray.com/freemint/freemint/$PACKAGE_PATH" | sudo tar xjf -
+	done
+	cd -
+
+	if [ "$CPU_TARGET" = "000" ]
+	then
+		DOSFSTOOLS_URL=$(curl -s -H "$CURL_HEADER" https://api.github.com/repos/freemint/dosfstools/releases/latest | jq -r '.assets[].browser_download_url' | grep 'm68000')
+	elif [ "$CPU_TARGET" = "col" ]
+	then
+		DOSFSTOOLS_URL=$(curl -s -H "$CURL_HEADER" https://api.github.com/repos/freemint/dosfstools/releases/latest | jq -r '.assets[].browser_download_url' | grep '5475')
+	else
+		# 02060, 030, 040, 060
+		DOSFSTOOLS_URL=$(curl -s -H "$CURL_HEADER" https://api.github.com/repos/freemint/dosfstools/releases/latest | jq -r '.assets[].browser_download_url' | grep 'm68020-60')
+	fi
+
+	cd .travis
+	mkdir dosfstools
+	wget -q -O - "$DOSFSTOOLS_URL" | tar xjf - -C dosfstools
+	cd -
 else
-# use an empty authorization header. this will fall back to 50 requests per hour from given Travis IP (for any project, not only ours!)
-CURL_HEADER=""
-CURL_USER=""
+	sudo apt-get install mintlib-m68k-atari-mint pml-m68k-atari-mint gemlib-m68k-atari-mint cflib-m68k-atari-mint gemma-m68k-atari-mint
 fi
-
-if [ "$CPU_TARGET" = "ara" ]
-then
-	CPU_TARGET="040"
-elif [ "$CPU_TARGET" = "prg" ]
-then
-	CPU_TARGET="000"
-fi
-
-LIBCMINI_URL=$(curl -s -H "$CURL_HEADER" https://api.github.com/repos/freemint/libcmini/releases/latest | jq -r '.assets[].browser_download_url')
-
-cd ..
-mkdir libcmini
-wget -q -O - "$LIBCMINI_URL" | tar xzf - -C libcmini
-cd -
-
-cd .travis
-for package in toswin2 qed cops hypview
-do
-	unset PACKAGE_VERSION
-	unset PACKAGE_PATH
-	PACKAGE_VERSION=$(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/freemint/$package" | jq -r '.latest_version')
-	read PACKAGE_PATH \
-		<<< $(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/freemint/$package/versions/$PACKAGE_VERSION/files" \
-			| jq -r '.[] | select(.name | endswith("-" + env.CPU_TARGET + ".zip")) | .path')
-	wget -q -O temp.zip "https://dl.bintray.com/freemint/freemint/$PACKAGE_PATH" && unzip -q temp.zip && rm temp.zip
-done
-cd -
-
-sudo mkdir -p "/usr/m68k-atari-mint"
-cd "/usr/m68k-atari-mint"
-for package in mintlib fdlibm gemlib cflib gemma
-do
-	unset PACKAGE_VERSION
-	unset PACKAGE_PATH
-	PACKAGE_VERSION=$(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/freemint/$package" | jq -r '.latest_version')
-	read PACKAGE_PATH \
-		<<< $(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/freemint/$package/versions/$PACKAGE_VERSION/files" \
-			| jq -r '.[].path')
-	wget -q -O - "https://dl.bintray.com/freemint/freemint/$PACKAGE_PATH" | sudo tar xjf -
-done
-cd -
-
-if [ "$CPU_TARGET" = "000" ]
-then
-	DOSFSTOOLS_URL=$(curl -s -H "$CURL_HEADER" https://api.github.com/repos/freemint/dosfstools/releases/latest | jq -r '.assets[].browser_download_url' | grep 'm68000')
-elif [ "$CPU_TARGET" = "col" ]
-then
-	DOSFSTOOLS_URL=$(curl -s -H "$CURL_HEADER" https://api.github.com/repos/freemint/dosfstools/releases/latest | jq -r '.assets[].browser_download_url' | grep '5475')
-else
-	# 02060, 030, 040, 060
-	DOSFSTOOLS_URL=$(curl -s -H "$CURL_HEADER" https://api.github.com/repos/freemint/dosfstools/releases/latest | jq -r '.assets[].browser_download_url' | grep 'm68020-60')
-fi
-
-cd .travis
-mkdir dosfstools
-wget -q -O - "$DOSFSTOOLS_URL" | tar xjf - -C dosfstools
-cd -
 
 # HCP is defined as "/tmp/hcp/bin/hcp"
-HCP_VERSION="1.0.3"
-# UDO is defined as "/tmp/udo/udo"
+HCP_VERSION="1.0.4"
+# UDO is defined as "/tmp/udo/bin/udo"
 UDO_VERSION="7.12"
 
 cd /tmp
 wget -q -O - "http://tho-otto.de/download/hcp-${HCP_VERSION}-linux.tar.bz2" | tar xjf -
 mv "hcp-${HCP_VERSION}" hcp
 
-mkdir -p "/tmp/udo"
-cd "/tmp/udo"
+mkdir -p "/tmp/udo/bin"
+cd "/tmp/udo/bin"
 wget -q -O - "http://www.tho-otto.de/download/udo-${UDO_VERSION}-linux.tar.bz2" | tar xjf -
--- script.sh
+++ script.sh
@@ -3,47 +3,53 @@
 # -x: Display expanded script commands
 
 export M68K_ATARI_MINT_CROSS=yes
-export LIBCMINI=yes
 
-SHORT_ID=$(git log -n1 --format="%H" | cut -c 1-3)
-PROJECT=$(echo "${TRAVIS_REPO_SLUG}" | cut -d '/' -f 2)
-
-HERE="$(dirname "$0")"
-. "$HERE/version.sh"
-
-ARANYM="${PWD}/.travis/aranym"
-export TERADESK_DIR="${PWD}/.travis/teradesk"
-export BASH_DIR="${PWD}/.travis/bash"
-export COREUTILS_DIR="${PWD}/.travis/coreutils"
-export QED_DIR="${PWD}/.travis/qed"
-export DOSFSTOOLS_DIR="${PWD}/.travis/dosfstools"
-export COPS_DIR="${PWD}/.travis/cops"
-export HYPVIEW_DIR="${PWD}/.travis/hypview"
-export TOSWIN2_DIR="${PWD}/.travis/toswin2"
-TMP="${PWD}/.travis/.tmp"
-OUT="${PWD}/.travis"
-
-mkdir -p "${OUT}"
-
-if [ "$CPU_TARGET" = "ara" ]
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_REPO_SLUG}" = "freemint/freemint" ]
 then
-	make
-	DST="${TMP}/aranym-${SHORT_VERSION}"
-	"./.travis/prepare-aranym.sh" "${PWD}" "${DST}" "${SHORT_VERSION}" "${ARANYM}"
-	find "${DST}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
-	cd "${DST}/.." && zip -r -9 "${OUT}/${PROJECT}-${SHORT_VERSION}-040${VERSIONED}.zip" "$(basename ${DST})" && cd -
-elif [ "$CPU_TARGET" = "prg" ]
-then
-	cd sys/usb && make && cd -
-	cd tools/usbtool && make all && cd -
-	DST="${TMP}/usb4tos-${SHORT_VERSION}"
-	"./.travis/prepare-usb4tos.sh" "${PWD}" "${DST}"
-	find "${DST}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
-	cd "${DST}/.." && zip -r -9 "${OUT}/usb4tos-${SHORT_VERSION}.zip" "$(basename ${DST})" && cd -
+	export LIBCMINI=yes
+
+	SHORT_ID=$(git log -n1 --format="%H" | cut -c 1-3)
+	PROJECT=$(echo "${TRAVIS_REPO_SLUG}" | cut -d '/' -f 2)
+
+	HERE="$(dirname "$0")"
+	. "$HERE/version.sh"
+
+	ARANYM="${PWD}/.travis/aranym"
+	export TERADESK_DIR="${PWD}/.travis/teradesk"
+	export BASH_DIR="${PWD}/.travis/bash"
+	export COREUTILS_DIR="${PWD}/.travis/coreutils"
+	export QED_DIR="${PWD}/.travis/qed"
+	export DOSFSTOOLS_DIR="${PWD}/.travis/dosfstools"
+	export COPS_DIR="${PWD}/.travis/cops"
+	export HYPVIEW_DIR="${PWD}/.travis/hypview"
+	export TOSWIN2_DIR="${PWD}/.travis/toswin2"
+	TMP="${PWD}/.travis/.tmp"
+	OUT="${PWD}/.travis"
+
+	mkdir -p "${OUT}"
+
+	if [ "$CPU_TARGET" = "ara" ]
+	then
+		make
+		DST="${TMP}/aranym-${SHORT_VERSION}"
+		"./.travis/prepare-aranym.sh" "${PWD}" "${DST}" "${SHORT_VERSION}" "${ARANYM}"
+		find "${DST}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
+		cd "${DST}/.." && zip -r -9 "${OUT}/${PROJECT}-${SHORT_VERSION}-040${VERSIONED}.zip" "$(basename ${DST})" && cd -
+	elif [ "$CPU_TARGET" = "prg" ]
+	then
+		cd sys/usb && make && cd -
+		cd tools/usbtool && make all && cd -
+		DST="${TMP}/usb4tos-${SHORT_VERSION}"
+		"./.travis/prepare-usb4tos.sh" "${PWD}" "${DST}"
+		find "${DST}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
+		cd "${DST}/.." && zip -r -9 "${OUT}/usb4tos-${SHORT_VERSION}.zip" "$(basename ${DST})" && cd -
+	else
+		make
+		DST="${TMP}/mint-${SHORT_VERSION}-${CPU_TARGET}"
+		"./.travis/prepare-snapshot.sh" "${PWD}" "${DST}" "${SHORT_VERSION}" "${SHORT_ID}"
+		find "${DST}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
+		cd "${DST}" && zip -r -9 "${OUT}/${PROJECT}-${SHORT_VERSION}-${CPU_TARGET}${VERSIONED}.zip" * && cd -
+	fi
 else
 	make
-	DST="${TMP}/mint-${SHORT_VERSION}-${CPU_TARGET}"
-	"./.travis/prepare-snapshot.sh" "${PWD}" "${DST}" "${SHORT_VERSION}" "${SHORT_ID}"
-	find "${DST}" -type f -perm -a=x -exec m68k-atari-mint-strip -s {} \;
-	cd "${DST}" && zip -r -9 "${OUT}/${PROJECT}-${SHORT_VERSION}-${CPU_TARGET}${VERSIONED}.zip" * && cd -
 fi
