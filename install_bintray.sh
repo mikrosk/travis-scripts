2af09103a9873f96e8109724b47c96132ced1943
Miro Kropacek <miro.kropacek@gmail.com>
2020-11-26 09:05:26 +0100

    Let BINTRAY_REPO to be overwritten

--- install_bintray.sh
+++ install_bintray.sh
@@ -5,6 +5,7 @@
 
 CURL_USER="${BINTRAY_USER}:${BINTRAY_API_KEY}"
 SYSROOT_DIR=${SYSROOT_DIR:-"/usr/m68k-atari-mint"}
+BINTRAY_REPO=${BINTRAY_REPO:-"freemint"}
 
 sudo mkdir -p "${SYSROOT_DIR}" && cd "${SYSROOT_DIR}"
 
@@ -12,9 +13,9 @@ for package in $*
 do
 	unset PACKAGE_VERSION
 	unset PACKAGE_PATH
-	PACKAGE_VERSION=$(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/freemint/$package" | jq -r '.latest_version')
+	PACKAGE_VERSION=$(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/$BINTRAY_REPO/$package" | jq -r '.latest_version')
 	read PACKAGE_PATH \
-		<<< $(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/freemint/$package/versions/$PACKAGE_VERSION/files" \
+		<<< $(curl -s -u "${CURL_USER}" -X GET "https://api.bintray.com/packages/freemint/$BINTRAY_REPO/$package/versions/$PACKAGE_VERSION/files" \
 			| jq -r '.[].path')
-	wget -q -O - "https://dl.bintray.com/freemint/freemint/$PACKAGE_PATH" | sudo tar xjf -
+	wget -q -O - "https://dl.bintray.com/freemint/$BINTRAY_REPO/$PACKAGE_PATH" | sudo tar xjf -
 done
